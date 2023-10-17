4a7261ac762202575130c0a3158d211ca6bbe3c9
Zuxuan Chen <62515929+ZuxuanChen@users.noreply.github.com>
2023-10-17 13:14:40 -0400

    Mod 7 route generate

---
+++ CSCI6212_Project/core/Home/views/HomeView.swift
@@ -0,0 +1,40 @@
+//
+//  HomeView.swift
+//  CSCI6212_Project
+//
+//  Created by zuxuan chen on 9/6/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showLocationSearchView = false
+    var body: some View {
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+                    
+            if showLocationSearchView{
+                LocationSearchView(showLocationSearchView: $showLocationSearchView)
+            }else{
+                LocationSearchActivationView()
+                    .padding(.top, 72)
+                    .onTapGesture{
+                        withAnimation(.spring()){
+                            showLocationSearchView.toggle()
+                        }
+                    }
+            }
+            
+            MapViewActionButton(showLocationSearchView: $showLocationSearchView)
+                .padding(.leading)
+                .padding(.top, 4)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
